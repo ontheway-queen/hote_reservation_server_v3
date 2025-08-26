1649da02973479d3988872acec2e0b35ab25694f
jobayerm360ict <jobayer.m360ict@gmail.com>
2025-08-26 10:33:23 +0600

    wip: hotel config setup

--- dist/appAdmin/services/subServices/siteConfigSupport.service.js
+++ dist/appAdmin/services/subServices/siteConfigSupport.service.js
@@ -13,8 +13,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SiteConfigSupportService = void 0;
-const abstract_service_1 = __importDefault(require("../../../abstract/abstract.service"));
-const pagesContent_1 = require("../../miscellaneous/siteConfig/pagesContent");
+const abstract_service_1 = __importDefault(require("../../../abstarcts/abstract.service"));
+const pagesContent_1 = require("../../../utils/miscellaneous/siteConfig/pagesContent");
 class SiteConfigSupportService extends abstract_service_1.default {
     constructor(trx) {
         super();
@@ -22,36 +22,32 @@ class SiteConfigSupportService extends abstract_service_1.default {
     }
     insertSiteConfigData({ agency_id, address, email, phone, site_name, logo, }) {
         return __awaiter(this, void 0, void 0, function* () {
-            const SiteConfigModel = this.Model.AgencyB2CConfigModel(this.trx);
+            const SiteConfigModel = this.Model.b2cConfigurationModel(this.trx);
             const payload = {
                 agency_id,
                 about_us_content: (0, pagesContent_1.aboutUsContent)(site_name, address),
                 emails: JSON.stringify([{ email }]),
                 numbers: JSON.stringify([{ number: phone }]),
-                address: JSON.stringify([{ title: 'Main Office', address }]),
+                address: JSON.stringify([{ title: "Main Office", address }]),
                 contact_us_content: (0, pagesContent_1.contactUsContent)(address, phone, email),
                 privacy_policy_content: (0, pagesContent_1.privacyAndPolicy)(site_name),
                 terms_and_conditions_content: (0, pagesContent_1.privacyAndPolicy)(site_name),
                 site_name,
                 hero_quote: `Welcome to ${site_name}!`,
-                hero_sub_quote: 'Find Flights, Hotels, Visa & Holidays',
+                hero_sub_quote: "Find Flights, Hotels, Visa & Holidays",
                 main_logo: logo,
-                site_thumbnail: 'agent/b2c/site-config/site-thumbnail.jpg',
-                about_us_thumbnail: 'agent/b2c/site-config/about-us.png',
-                contact_us_thumbnail: 'agent/b2c/site-config/contact-us.jpg',
-                favicon: 'agent/b2c/site-config/favicon.png',
+                site_thumbnail: "agent/b2c/site-config/site-thumbnail.jpg",
+                about_us_thumbnail: "agent/b2c/site-config/about-us.png",
+                contact_us_thumbnail: "agent/b2c/site-config/contact-us.jpg",
+                favicon: "agent/b2c/site-config/favicon.png",
                 meta_description: `Welcome to ${site_name}, The ultimate online platform for ota services! Book air tickets, hotels, visa, tour package without hassle at the most competitive rates.`,
                 meta_tags: `${site_name}, flight booking, cheap hotels, tour packages, visa services, travel deals, online travel agency`,
                 meta_title: ` ${site_name} | Book Flights, Hotel, Tour, Visa Online`,
                 notice: `Welcome to ${site_name}`,
             };
             yield SiteConfigModel.insertSiteConfig(payload);
-            const popularDestPayload = (0, pagesContent_1.popularDestination)(agency_id);
-            const popularPlacesPayload = (0, pagesContent_1.popularPlaces)(agency_id);
             const heroBGPayload = (0, pagesContent_1.heroBG)(agency_id);
             yield SiteConfigModel.insertHeroBGContent(heroBGPayload);
-            yield SiteConfigModel.insertPopularDestination(popularDestPayload);
-            yield SiteConfigModel.insertPopularPlaces(popularPlacesPayload);
         });
     }
 }
--- src/appAdmin/services/subServices/siteConfigSupport.service.ts
+++ src/appAdmin/services/subServices/siteConfigSupport.service.ts
@@ -1,19 +1,11 @@
-import { Knex } from 'knex';
-import AbstractServices from '../../../abstract/abstract.service';
-import {
-  ICreateAgencyB2CHeroBgContentPayload,
-  ICreateAgencyB2CPopularDestinationPayload,
-  ICreateAgencyB2CPopularPlace,
-  ICreateAgencyB2CSiteConfig,
-} from '../../modelTypes/agencyB2CModelTypes/agencyB2CConfigModel.types';
+import { Knex } from "knex";
+import AbstractServices from "../../../abstarcts/abstract.service";
 import {
   aboutUsContent,
   contactUsContent,
   heroBG,
-  popularDestination,
-  popularPlaces,
   privacyAndPolicy,
-} from '../../miscellaneous/siteConfig/pagesContent';
+} from "../../../utils/miscellaneous/siteConfig/pagesContent";
 
 export class SiteConfigSupportService extends AbstractServices {
   private trx: Knex.Transaction;
@@ -37,25 +29,25 @@ export class SiteConfigSupportService extends AbstractServices {
     email: string;
     logo: string;
   }) {
-    const SiteConfigModel = this.Model.AgencyB2CConfigModel(this.trx);
+    const SiteConfigModel = this.Model.b2cConfigurationModel(this.trx);
 
     const payload: ICreateAgencyB2CSiteConfig = {
       agency_id,
       about_us_content: aboutUsContent(site_name, address),
       emails: JSON.stringify([{ email }]),
       numbers: JSON.stringify([{ number: phone }]),
-      address: JSON.stringify([{ title: 'Main Office', address }]),
+      address: JSON.stringify([{ title: "Main Office", address }]),
       contact_us_content: contactUsContent(address, phone, email),
       privacy_policy_content: privacyAndPolicy(site_name),
       terms_and_conditions_content: privacyAndPolicy(site_name),
       site_name,
       hero_quote: `Welcome to ${site_name}!`,
-      hero_sub_quote: 'Find Flights, Hotels, Visa & Holidays',
+      hero_sub_quote: "Find Flights, Hotels, Visa & Holidays",
       main_logo: logo,
-      site_thumbnail: 'agent/b2c/site-config/site-thumbnail.jpg',
-      about_us_thumbnail: 'agent/b2c/site-config/about-us.png',
-      contact_us_thumbnail: 'agent/b2c/site-config/contact-us.jpg',
-      favicon: 'agent/b2c/site-config/favicon.png',
+      site_thumbnail: "agent/b2c/site-config/site-thumbnail.jpg",
+      about_us_thumbnail: "agent/b2c/site-config/about-us.png",
+      contact_us_thumbnail: "agent/b2c/site-config/contact-us.jpg",
+      favicon: "agent/b2c/site-config/favicon.png",
       meta_description: `Welcome to ${site_name}, The ultimate online platform for ota services! Book air tickets, hotels, visa, tour package without hassle at the most competitive rates.`,
       meta_tags: `${site_name}, flight booking, cheap hotels, tour packages, visa services, travel deals, online travel agency`,
       meta_title: ` ${site_name} | Book Flights, Hotel, Tour, Visa Online`,
@@ -64,18 +56,8 @@ export class SiteConfigSupportService extends AbstractServices {
 
     await SiteConfigModel.insertSiteConfig(payload);
 
-    const popularDestPayload: ICreateAgencyB2CPopularDestinationPayload[] =
-      popularDestination(agency_id);
-
-    const popularPlacesPayload: ICreateAgencyB2CPopularPlace[] =
-      popularPlaces(agency_id);
-
-    const heroBGPayload: ICreateAgencyB2CHeroBgContentPayload[] =
-      heroBG(agency_id);
+    const heroBGPayload: ICreateB2CHeroBgContentPayload[] = heroBG(agency_id);
 
     await SiteConfigModel.insertHeroBGContent(heroBGPayload);
-    await SiteConfigModel.insertPopularDestination(popularDestPayload);
-
-    await SiteConfigModel.insertPopularPlaces(popularPlacesPayload);
   }
 }
